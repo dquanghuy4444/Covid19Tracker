03a63e37d2a011ab70dd5be799d0579d1d7b7224
dquanghuy4444 <wepiohip@gmail.com>
2020-10-16 11:39:57 +0700

    Updates

---
+++ precache-manifest.5a344196b97b9401f6707df2993fc45f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463b7ba1d549577531fc3da9be2e7f98",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "72db83c6c84d17ffb90c",
+    "url": "/covid19-tracker/static/css/main.c3460a9b.chunk.css"
+  },
+  {
+    "revision": "d57ef544bdc6b5638240",
+    "url": "/covid19-tracker/static/js/2.f812f317.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19-tracker/static/js/2.f812f317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72db83c6c84d17ffb90c",
+    "url": "/covid19-tracker/static/js/main.63265a06.chunk.js"
+  },
+  {
+    "revision": "13fb7db908453bcc700b",
+    "url": "/covid19-tracker/static/js/runtime-main.1e63171d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
